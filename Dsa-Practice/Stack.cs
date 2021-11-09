7623713b050463590d481e28a94a184fc5fdcf63
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-11-09 13:29:41 +0545

    Stack ds

---
+++ Dsa-Practice/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsa_Practice
+{
+
+    public class Stack<T>   // Stack -> LIFO principle
+    {
+        Entry top;      // properties of Stack 'top' of Entry type
+
+        public void Push(T data)
+        {
+            top = new Entry(top, data);
+        }
+
+        public T Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T result = top.Data;
+            top = top.Next;
+            return result;
+        }
+
+        class Entry     // none modifier allows access for class and namespace
+        {
+            public Entry Next
+            {
+                get;
+                set;
+            }
+
+            public T Data
+            {
+                get;
+                set;
+            }
+
+            public Entry(Entry next, T data)
+            {
+                this.Next = next;
+                this.Data = data;
+            }
+
+
+        }
+    }
+}
+
+
+
+
---
+++ Dsa-Practice/StackMainMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dsa_Practice
+{
+    class StackMainMethod
+    {
+        static void Main(string[] args)
+        {
+            var st = new Stack<int>();
+            st.Push(12);
+            st.Push(1);
+            st.Push(13);
+            st.Push(98);
+            st.Push(199);
+
+            Console.WriteLine(st);
+            Console.WriteLine(st.Pop());   // remove last item(199) and writeLine method returns it
+
+            var forStr = new Stack<string>();
+            forStr.Push("Number");
+            forStr.Push("Number 1");
+            forStr.Push("Number 2");
+            Console.WriteLine(forStr.Pop());  // remove last item(Number 2) and writeLine method returns it
+
+        }
+    }
+}
